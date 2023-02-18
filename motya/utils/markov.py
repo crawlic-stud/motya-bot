0c8a56ed8b52e2f145c75667740e8c566fe55e41
crawlic-stud <nikitosik0726@gmail.com>
2023-02-18 22:18:30 +0300

    one more bugfix when no saved messages in db

--- motya/utils/markov.py
+++ motya/utils/markov.py
@@ -3,12 +3,14 @@ from markovify import NewlineText
 
 # TODO: refactor those functions
 def generate_random_sentence(messages_list):
+    if not messages_list:
+        return
+    
     # dumping all db data to string
     text = ''
     for message in messages_list:
         text += message + '\n'
 
-    # print(text)
     text_model = NewlineText(input_text=text, well_formed=False, state_size=1)
     sentence = text_model.make_sentence(tries=1000)
 
