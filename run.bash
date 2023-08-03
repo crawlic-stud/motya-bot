ee274b15a600d1d455ba0ad7be92f47e30f0575b
crawlic-stud <nikitosik0726@gmail.com>
2023-08-04 00:57:43 +0300

    dockerized motya

--- run.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 /root/bots/motya-bot/motya/main.py &
-echo "motya-bot started"
--- stop.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pkill -f /root/bots/motya-bot/motya/main.py
-echo "motya-bot stopped"
